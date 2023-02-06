f7029f7f9981dce78a3684f14f32d9fa28d7aa27
duckbanni <vincent.lanore@gmail.com>
2023-02-06 12:14:14 +0000

    RichText.rend now returns tooltips

--- troubadour/rich_text.py
+++ troubadour/rich_text.py
@@ -14,9 +14,18 @@ class RichText:
     args: list["RichText"] = field(default_factory=list)
     kwargs: dict[str, "RichText"] = field(default_factory=dict)
 
-    def render(self, markdown: bool = True) -> str:
-        rendered_args = [arg.render(False) for arg in self.args]
-        rendered_kwargs = {key: arg.render(False) for key, arg in self.kwargs.items()}
+    def render(self, markdown: bool = True) -> tuple[str, dict[str, str]]:
+        tooltips: dict[str, str] = {}
+        rendered_args = []
+        rendered_kwargs = {}
+        for arg in self.args:
+            rendered_arg, rec_tooltips = arg.render(False)
+            rendered_args.append(rendered_arg)
+            tooltips |= rec_tooltips
+        for kw, arg in self.kwargs.items():
+            rendered_arg, rec_tooltips = arg.render(False)
+            rendered_kwargs[kw] = rendered_arg
+            tooltips |= rec_tooltips
         result = self.text.format(*rendered_args, **rendered_kwargs)
         html_params = ""
         if self.classes:
@@ -24,11 +33,13 @@ class RichText:
         if self.tooltip is not None:
             id = get_id()
             html_params += f' id="troubadour_tooltip_{id}"'
+            tooltips[f"troubadour_tooltip_{id}"], rec_tooltips = self.tooltip.render()
+            tooltips |= rec_tooltips
         if html_params != "":
             result = f"<span{html_params}>{result}</span>"
         if markdown:
             result = mistune.html(result)
-        return result
+        return result, tooltips
 
 
 if __name__ == "__main__":

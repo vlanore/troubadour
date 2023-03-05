a53531520b3b051b2aaa4624b92412e097c95063
duckbanni <vincent.lanore@gmail.com>
2023-03-05 16:54:17 +0000

    Selenium test improved + basic richtext test

--- troubadour/__init__.py
+++ troubadour/__init__.py
@@ -1,16 +1,11 @@
-from troubadour.html_impl import ImagePanel, InfoPanel, Story, run_game
 from troubadour.interfaces import Button, Game, Input, TextInput, Inputs
 from troubadour.rich_text import RichText
 
 __all__ = [
     "Button",
     "Game",
-    "ImagePanel",
-    "InfoPanel",
     "Input",
     "Inputs",
     "RichText",
-    "run_game",
-    "Story",
     "TextInput",
 ]

34a088bead3780cef5146747dea5419c3e0185ad
duckbanni <vincent.lanore@gmail.com>
2023-03-05 15:12:36 +0000

    Slightly improved selenium test

--- test/test_all.py
+++ test/test_all.py
@@ -6,12 +6,23 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_launch() -> None:
-    opts: FirefoxOptions = FirefoxOptions()  # type:ignore
-    opts.add_argument("--headless")  # type:ignore
-    driver = webdriver.Firefox(options=opts)  # type:ignore
+    opts: FirefoxOptions = FirefoxOptions()
+    opts.add_argument("--headless")
+    driver = webdriver.Firefox(options=opts)
 
     driver.get("localhost:8888/build")
     WebDriverWait(driver, 30).until(
         EC.presence_of_element_located((By.ID, "troubadour_tooltip_0"))
     )
-    assert "doomed" in driver.find_element(By.ID, "story").text
+
+    story = driver.find_element(By.ID, "story")
+    story_interface = driver.find_element(By.ID, "story-interface")
+
+    assert "doomed" in story.text
+    buttons = story_interface.find_elements(By.CLASS_NAME, "button")
+    assert len(buttons) == 1
+    buttons[0].click()
+
+    assert "sit amet sodales" in story.text
+    buttons = story_interface.find_elements(By.CLASS_NAME, "button")
+    assert len(buttons) == 2

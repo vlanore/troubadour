314d488189d4948de72895e0a46cc97470ec5b2c
duckbanni <vincent.lanore@gmail.com>
2023-03-05 22:18:40 +0000

    Fixed interactive tippy tooltips breaking test (but nested tooltips still broken)

--- troubadour/pyscript_render.py
+++ troubadour/pyscript_render.py
@@ -52,6 +52,7 @@ def add_tooltip(id: str, text: str) -> None:
                     content:`{text}`,
                     allowHTML:true,
                     interactive: true,
+                    appendTo: document.body,
                 }}
             );"""
     )
